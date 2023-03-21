2f33905d3c863c3a2b5cfd58d68f03e4a7928d7a
DrF33lg00d <7drf33lg00d7@gmail.com>
2023-03-21 23:16:05 +0300

    Add peewee ORM library and new tables

--- db.py
+++ db.py
@@ -1,6 +1,41 @@
 import sqlite3
 
-from settings import db_settings
+from peewee import (
+    Model, PrimaryKeyField, CharField, ForeignKeyField, BooleanField,
+    Database,
+    )
+
+from settings import db, db_settings
+
+
+class DefaultModel(Model):
+    class Meta:
+        database = db
+
+
+class PosCat(DefaultModel):
+    id = PrimaryKeyField()
+    description = CharField()
+
+
+class Question(DefaultModel):
+    id = PrimaryKeyField()
+    description = CharField()
+    poscat = ForeignKeyField(PosCat, backref='poscat')
+
+
+class Answer(DefaultModel):
+    id = PrimaryKeyField()
+    description = CharField()
+    is_correct = BooleanField(default=False)
+
+
+def init_tables(db: Database = db) -> None:
+    db.create_tables([PosCat, Question, Answer])  # type: ignore
+    return None
+
+
+init_tables()
 
 
 class DB:
--- settings.py
+++ settings.py
@@ -1,11 +1,18 @@
+import os
+
+from peewee import SqliteDatabase
+
 from contextlib import suppress
 
 db_settings = {
     'name': 'result.db'
 }
 
+db = SqliteDatabase(os.path.join(os.getcwd(), db_settings['name']))
+
 url = 'https://sobaka[.]babaka[.]xyz'
 id = 100
+
 
 with suppress(ImportError):
     from local_settings import *
