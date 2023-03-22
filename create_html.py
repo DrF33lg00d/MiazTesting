f4529531c27eaec64bd70ebcadda7ceec1b7d8a4
DrF33lg00d <7drf33lg00d7@gmail.com>
2023-03-22 21:15:39 +0300

    Update crete_html.py

--- create_html.py
+++ create_html.py
@@ -1,26 +1,40 @@
-from db import db
+from db import PosCat, Question, Answer, init_tables
+from utils.settings import logger, POSCAT_ID
 
 
 if '__main__' == __name__:
+    init_tables()
     print('Create file')
-    questions = db.get_all_questions()
-    questions.sort(key=lambda x: x['description'])
-    for question in questions:
-        question['answers'] = db.get_all_answers(question['id'])
-    with open('answers.html', 'w', encoding='cp1251') as f:
-        f.write('<html>')
-        f.write('<head>')
-        f.write('<title>Answers</title>')
-        f.write('</head>')
+
+    logger.info('Start creating file with answers.html')
+    poscat: PosCat = PosCat.get(id=POSCAT_ID)
+    if not poscat:
+        logger.critical(f'Cat_id {POSCAT_ID} not found!')
+
+    questions: list[Question] = [que for que in (Question
+         .select(Question, PosCat)
+         .join(PosCat)
+         .where(Question.poscat == poscat)
+         .order_by(Question.description.asc())
+         )]
+    with open('answers.html', 'w', encoding='utf-8') as f:
+        f.write('<html><head><title>Answers</title></head>')
 
         f.write('<body>')
         for question in questions:
-            f.write(f'<em>{question["description"]}</em><br>')
-            f.write('<ul>')
-            for answer in question['answers']:
-                text = f'<b>{answer["description"]}</b>' if answer['is_correct'] else answer['description']
-                f.write(f'<li>{text}</li>')
-            f.write('</ul>')
-            f.write('<br>')
+            answers: list[Answer] = [answer for answer in (Answer
+                .select(Answer, Question)
+                .join(Question)
+                .where(Answer.question == question)
+                .order_by(Answer.id.asc())
+                )]
+            for question in questions:
+                f.write(f'<em>{question.description}</em><br>')
+                f.write('<ul>')
+                for answer in answers:
+                    text = f'<b>{answer.description}</b>' if answer.is_correct else answer.description
+                    f.write(f'<li>{text}</li>')
+                f.write('</ul>')
+                f.write('<br>')
         f.write('</body>')
         f.write('</html>')
